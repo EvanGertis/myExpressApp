aa4d6d34399cc03e68cd7276c00aecafc537100d
Evan Gertis <physevangertis@gmail.com>
2021-10-13 12:59:38 -0400

    add ziti methods from gitlab example

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,88 @@ var express = require('express');
 var router = express.Router();
 const ziti   = require('ziti-sdk-nodejs');
 
+const UV_EOF = -4095;
+
+const zitiInit = async (zitiFile) => {
+  return new Promise((resolve, reject) => {
+    var rc = ziti.ziti_init(zitiFile, (init_rc) => {
+        if (init_rc < 0) {
+            return reject(`init_rc = ${init_rc}`);
+        }
+        return resolve();
+    });
+
+    if (rc < 0) {
+        return reject(`rc = ${rc}`);
+    }
+  });
+};
+
+const zitiServiceAvailable = async (service) => {
+  return new Promise((resolve, reject) => {
+    ziti.ziti_service_available(service, (obj) => {
+      if (obj.status != 0) {
+        console.log(`service ${service} not available, status: ${status}`);
+        return reject(status);
+      } else {
+        console.log(`service ${service} available`);
+        return resolve();
+      }
+    });
+  });
+}
+
+const zitiHttpRequest = async (url, method, headers) => {
+  return new Promise((resolve) => {
+    ziti.Ziti_http_request(
+      url, 
+      method,
+      headers,
+      (obj) => { // on_req callback
+          console.log('on_req callback: req is: %o', obj.req);
+          return resolve(obj.req);
+      },        
+      (obj) => { // on_resp callback
+        console.log(`on_resp status: ${obj.code} ${obj.status}`);
+        if (obj.code != 200) {
+          core.setFailed(`on_resp failure: ${obj.status}`);
+          process.exit(-1);
+        }
+        process.exit(0);
+      },
+      (obj) => { // on_resp_body callback
+        // not expecting any body...
+        if (obj.len === UV_EOF) {
+          console.log('response complete')
+          process.exit(0);
+        } else if (obj.len < 0) {
+          core.setFailed(`on_resp failure: ${obj.len}`);
+          process.exit(-1);
+        }
+
+        if (obj.body) {
+          let str = Buffer.from(obj.body).toString();
+          console.log(`on_resp_body len: ${obj.len}, body: ${str}`);
+        } else {
+          console.log(`on_resp_body len: ${obj.len}`);
+        }
+      });
+  });
+};
+
+const zitiHttpRequestData = async (req, buf) => {
+  ziti.Ziti_http_request_data(
+    req, 
+    buf,
+    (obj) => { // on_req_body callback
+      if (obj.status < 0) {
+          reject(obj.status);
+      } else {
+          resolve(obj);
+      }
+  });
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
